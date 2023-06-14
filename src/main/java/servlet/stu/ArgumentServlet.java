982654d1168ffd310ef66bc82a1c9d41604c7ba5
cywscs <cywscs@qq.com>
2023-06-14 10:44:11 +0800

    completed

--- src/main/java/servlet/stu/ArgumentServlet.java
+++ src/main/java/servlet/stu/ArgumentServlet.java
@@ -1,16 +1,35 @@
 package servlet.stu;
 
 import com.alibaba.fastjson.JSONObject;
+import entity.Homework;
+import entity.StuWork;
+import util.JdbcUtil;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
+
+@WebServlet("/argument")
 public class ArgumentServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        Integer stuid = Integer.parseInt(id);
+        String argument = req.getParameter("argument");
+
+        String sql1 = "select * from stu_work where id = ?";
+        List<StuWork> stuWork = JdbcUtil.queryList(StuWork.class, sql1, stuid);
+        StuWork work = stuWork.get(0);
+        JdbcUtil.exeUpdate("delete from stu_work where id = ?", stuid);
+        String sql2 = "insert into stu_work value(?, ?, ?, ?, ?, ?, ?, ? ,?)";
+        JdbcUtil.exeUpdate(sql2, work.getId(), work.getWork_id(), work.getStu_id(), work.getAttachment(),
+                work.getAnswer(), work.getStatus(), work.getScore(), work.getComment(), argument);
+
         JSONObject respJson = new JSONObject();
         respJson.put("code", 200);
         respJson.put("msg", "success");
--- src/main/java/servlet/stu/SubmitServlet.java
+++ src/main/java/servlet/stu/SubmitServlet.java
@@ -1,19 +1,53 @@
 package servlet.stu;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 
 @WebServlet("/submit")
+//@MultipartConfig
 public class SubmitServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        //  multipart/form-data
+        boolean isMultipart = ServletFileUpload.isMultipartContent(req);
+        if (!isMultipart) {
+            System.out.println("ok");
+        }else{
+            try {
+                List<FileItem> fileItems = upload.parseRequest(req);
+                for (FileItem fileItem : fileItems) {
+                    if (fileItem.isFormField()) {
+                        System.out.println(fileItem.getFieldName()+"\t"+fileItem.getString("UTF-8"));
+                    } else {
+                        String fileName = fileItem.getName();
+                        System.out.println(fileName);
+                        String ext = fileName.substring(fileName.lastIndexOf("."));
+                        String name = java.util.UUID.randomUUID()+ext;
+                        File file = new File("/home/cywscs" + File.separator +name);
+                        fileItem.write(file);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         JSONObject respJson = new JSONObject();
         respJson.put("code", 200);
         respJson.put("msg", "success");
